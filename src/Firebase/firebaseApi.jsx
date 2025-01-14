4a1b0e9f304a89a53295d227679f4a70f326a849
Nhlakanipho <nhlakaniphoradebe337@gmail.com>
2025-01-14 13:11:16 +0100

    Installations: installed firebase dependencies

---
+++ src/Firebase/firebaseApi.jsx
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCllB1AExMwsOtniJ9iY5q6s27hPDvTXu8",
+  authDomain: "goquest-47c9a.firebaseapp.com",
+  projectId: "goquest-47c9a",
+  storageBucket: "goquest-47c9a.firebasestorage.app",
+  messagingSenderId: "583000744790",
+  appId: "1:583000744790:web:46175b66393ea796c1ecf7",
+  measurementId: "G-JN1005DCB8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
