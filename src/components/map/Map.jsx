4915dffe8242943f9700be2a54c4934914bc2f09
MandlakheM <142303272+MandlakheM@users.noreply.github.com>
2025-01-14 14:38:34 +0200

    page layout created

---
+++ src/components/map/Map.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./map.css";
+function Map() {
+  return (
+    <div className="mapContainer ">
+      <p>Map here</p>
+    </div>
+  );
+}
+
+export default Map;
